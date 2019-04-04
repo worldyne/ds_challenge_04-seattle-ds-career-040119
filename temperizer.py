14d7a5511ddeda26d6b70cc691e4ec62de2a881d
Lawrence Chew <larrychewy@gmail.com>
2019-04-04 09:30:15 -0700

    Added docstrings describing functions

--- temperizer.py
+++ temperizer.py
@@ -13,6 +13,7 @@ def convert_c_to_f(temperature_c):
     return temperature_f
 
 def convert_c_to_k(temperature_c):
+    '''Convert Celcius to Kelvin'''
     temperature_k = temperature_c + 273.15
     if temperature_k < 0:
         return 'Kelvin can\'t be below 0'
@@ -20,18 +21,21 @@ def convert_c_to_k(temperature_c):
 
     
 def convert_k_to_c(temperature_k):
+    '''Convert Kelvin to Celcius'''
     if temperature_k < 0:
         return 'Kelvin can\'t be below 0'
     temperature_c = temperature_k - 273.15
     return temperature_c
 
 def convert_f_to_k(temperature_f):
+    '''Convert Farenheit to Kelvin'''
     temperature_k = convert_f_to_c(temperature_f) + 273.15
     if temperature_k < 0:
         return 'Kelvin can\'t be below 0'
     return temperature_k
 
 def convert_k_to_f(temperature_k):
+    '''Convert Kelvin to Farenheit'''
     if temperature_k < 0:
         return 'Kelvin can\'t be below 0'
     temperature_f = convert_c_to_f(temperature_k - 273.15)
